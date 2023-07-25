3431dd7b02feb559e6947738ad55b605d0ac7f1c
DEVIKA S KRISHNAN <devikaskrishnan2018@gmail.com>
2023-07-25 17:36:37 +0530

    add notes features

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'jquery-rails'
 gem 'simplecov', require: false
 gem 'kaminari'
 gem 'jquery-ui-rails'
+gem 'ransack'
+
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
       put :assign_team
     end
   end
-
   resources :teams do
     member do
       post :join
@@ -14,7 +13,6 @@ Rails.application.routes.draw do
       delete :destroy, action: :delete_team, as: :delete_team
       post :create_note
     end
-
     delete :leave_team, on: :member, as: :leave_team
   end
   resources :users do
@@ -23,13 +21,9 @@ Rails.application.routes.draw do
   resources :notes
   delete '/notes/:id', to: 'notes#destroy', as: :delete_note
   patch '/teams/:team_id/notes/:id/update_note_access', to: 'notes#update_note_access', as: :update_note_access_team_note
-
-
   patch 'users/:id/update_role', to: 'users#update_role', as: :update_role
   get '/dashboard', to: 'dashboard#index', as: :dashboard
-
   get '/your_teams', to: 'teams#your_teams', as: :your_teams
   get '/joined_teams', to: 'teams#joined_teams', as: :joined_teams
-
   root 'dashboard#index'
 end
