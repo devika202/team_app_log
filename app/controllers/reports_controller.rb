9af560b0e85ceb9011a0df9a7e0db2e19259a61c
DEVIKA S KRISHNAN <devikaskrishnan2018@gmail.com>
2023-08-07 14:29:57 +0530

    add pending changes

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,16 +1,16 @@
 class ReportsController < ApplicationController
   def visited_urls
-    user_behaviors = UserBehavior.where(user_id: current_user.id).order(:timestamp)
+    user_behaviors = UserBehavior.where(user_id: current_user.id).order(timestamp: :desc)
 
     @visited_urls = []
 
     user_behaviors.each_with_index do |behavior, index|
       url = behavior.page_name
-      timestamp = behavior.timestamp
+      timestamp = behavior.timestamp.in_time_zone('Asia/Kolkata')  
 
       if index > 0
         previous_timestamp = user_behaviors[index - 1].timestamp
-        time_spent = (timestamp - previous_timestamp).to_i
+        time_spent = (previous_timestamp - timestamp).to_i  
         formatted_time_spent = format_time(time_spent)
         @visited_urls << { url: url, timestamp: timestamp, time_spent: formatted_time_spent }
       else
